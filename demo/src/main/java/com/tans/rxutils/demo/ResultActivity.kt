4ad485d744256fb4ff6b3c72bfddb04836b3ab60
tanpengcheng <tanpengcheng@inke.cn>
2023-09-28 11:02:00 +0800

    Upgrade dependencies.

--- demo/src/main/java/com/tans/rxutils/demo/ResultActivity.kt
+++ demo/src/main/java/com/tans/rxutils/demo/ResultActivity.kt
@@ -4,7 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.jakewharton.rxbinding3.view.clicks
+import com.jakewharton.rxbinding4.view.clicks
 import kotlinx.android.synthetic.main.activity_result.*
 
 /**
